9f8b3a48c94c1faea93be56fc06daa6badbbad73
Raul Camacho <osvaldoracm@gmail.com>
2020-02-10 17:45:38 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Frase from './components/Frase';
+
 
 function App() {
+
+
+  //Crear el state
+  const [frase, obtenerFrase] = useState({});
+
+  //consulta a una API
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    // agregar el resultado de la API al state
+    obtenerFrase(resultado.data[0]);
+  }
+
+  useEffect(() => {
+    consultarAPI()
+  },[obtenerFrase])
+
+  console.log(frase);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="contenedor">
+    <Frase
+    frase={frase}
+    />
+    <button onClick={consultarAPI} >Generar nueva</button>
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Frase({ frase }) {
+
+    return (
+        <div className="frase">
+            <h1>{frase.quote}</h1>
+            <p>-{frase.author}</p>
+        </div>
+    )
+}
+
+export default Frase;
